fa7f9178a728298bae5db14a91b374741042de18
Ksenia Melnyk <melnyksenia@gmail.com>
2024-09-10 07:20:35 +0200

    1st try

---
+++ js/task-1.js
@@ -0,0 +1,20 @@
+//Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
+
+// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+// Усі символи slug повинні бути в нижньому регістрі.
+// Усі слова slug повинні бути розділені тире.
+
+function slugify(title) {
+    const titleL = title.toLowerCase();
+    
+    const args = titleL.split(" ");
+    const argsjoined = args.join("-");
+    
+    return argsjoined;
+}
+
+
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
